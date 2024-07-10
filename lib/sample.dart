0fb4608e26f51332f22c945e48e0d503f4eaf62a
Kim Paolo R. Cuenca <Kim Paolo R. Cuenca@LAPTOP-2IA9591B>
2024-07-10 16:47:36 +0800

    database

--- lib/sample.dart
+++ lib/sample.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'expense_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ExpenseAdapter());
+  await Hive.openBox<Expense>('expenses');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -42,23 +48,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  List<Map<String, dynamic>> _expenses = [];
+  Box<Expense>? _expenseBox;
+  List<Expense> _expenses = [];
   double _totalMonthlyAmount = 0.0;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    _expenseBox = Hive.box<Expense>('expenses');
+    _fetchExpenses();
   }
+
+  void _fetchExpenses() {
+    final expenses = _expenseBox?.values.toList() ?? [];
+    double totalAmount = expenses.fold(0, (sum, item) => sum + item.amount);
+    setState(() {
+      _expenses = expenses;
+      _totalMonthlyAmount = totalAmount;
+    });
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -93,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage>
           );
         },
       ),
-      floatingActionButton: _tabController.index == 0
+floatingActionButton: _tabController.index == 0
           ? FloatingActionButton(
               onPressed: () {
                 _showExpenseEntryDialog(context);
@@ -101,105 +119,104 @@ class _MyHomePageState extends State<MyHomePage>
               child: const Icon(Icons.add),
             )
           : null,
+
     );
   }
 
   Widget _buildHomeTab() {
-  List<double> monthlyExpenses = List.filled(12, 0.0);
+    List<double> monthlyExpenses = List.filled(12, 0.0);
 
-  for (var expense in _expenses) {
-    int month = expense['date'].month;
-    monthlyExpenses[month - 1] += expense['amount'];
-  }
+    for (var expense in _expenses) {
+      int month = expense.date.month;
+      monthlyExpenses[month - 1] += expense.amount;
+    }
 
-  // Format total monthly amount with commas
-  String formattedTotalAmount =
-      NumberFormat.currency(locale: 'en_PH', symbol: '₱').format(_totalMonthlyAmount);
+    String formattedTotalAmount =
+        NumberFormat.currency(locale: 'en_PH', symbol: '₱').format(_totalMonthlyAmount);
 
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          'Total Expenses this month: $formattedTotalAmount',
-          style: const TextStyle(
-            fontSize: 24,
-            fontFamily: 'BebasNeue',
-          ),
-        ),
-      ),
-      Expanded(
-        child: Padding(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
           padding: const EdgeInsets.all(8.0),
-          child: BarChart(
-            BarChartData(
-              alignment: BarChartAlignment.spaceAround,
-              maxY: monthlyExpenses.reduce((value, element) => value > element ? value : element) * 1.5,
-              gridData: FlGridData(show: false),
-              titlesData: FlTitlesData(
-                bottomTitles: SideTitles(
-                  showTitles: true,
-                  getTextStyles: (context, value) => const TextStyle(
-                    fontSize: 14, // Adjust the font size here
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                  getTitles: (value) {
-                    switch (value.toInt()) {
-                      case 0:
-                        return 'Jan';
-                      case 1:
-                        return 'Feb';
-                      case 2:
-                        return 'Mar';
-                      case 3:
-                        return 'Apr';
-                      case 4:
-                        return 'May';
-                      case 5:
-                        return 'Jun';
-                      case 6:
-                        return 'Jul';
-                      case 7:
-                        return 'Aug';
-                      case 8:
-                        return 'Sep';
-                      case 9:
-                        return 'Oct';
-                      case 10:
-                        return 'Nov';
-                      case 11:
-                        return 'Dec';
-                      default:
-                        return '';
-                    }
-                  },
-                ),
-                leftTitles: SideTitles(
-                  showTitles: true,
-                  getTextStyles: (context, value) => const TextStyle(
-                    fontSize: 10, // Adjust the font size here
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                  reservedSize: 30,
-                ),
-                topTitles: SideTitles(showTitles: false),
-              ),
-              borderData: FlBorderData(
-                show: true,
-                border: Border.all(color: Colors.black),
-              ),
-              barGroups: _generateBars(monthlyExpenses),
+          child: Text(
+            'Total Expenses this month: $formattedTotalAmount',
+            style: const TextStyle(
+              fontSize: 24,
+              fontFamily: 'BebasNeue',
             ),
           ),
         ),
-      ),
-    ],
-  );
-}
-
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.spaceAround,
+                maxY: monthlyExpenses.reduce((value, element) => value > element ? value : element) * 1.5,
+                gridData: FlGridData(show: false),
+                titlesData: FlTitlesData(
+                  bottomTitles: SideTitles(
+                    showTitles: true,
+                    getTextStyles: (context, value) => const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    getTitles: (value) {
+                      switch (value.toInt()) {
+                        case 0:
+                          return 'Jan';
+                        case 1:
+                          return 'Feb';
+                        case 2:
+                          return 'Mar';
+                        case 3:
+                          return 'Apr';
+                        case 4:
+                          return 'May';
+                        case 5:
+                          return 'Jun';
+                        case 6:
+                          return 'Jul';
+                        case 7:
+                          return 'Aug';
+                        case 8:
+                          return 'Sep';
+                        case 9:
+                          return 'Oct';
+                        case 10:
+                          return 'Nov';
+                        case 11:
+                          return 'Dec';
+                        default:
+                          return '';
+                      }
+                    },
+                  ),
+                  leftTitles: SideTitles(
+                    showTitles: true,
+                    getTextStyles: (context, value) => const TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    reservedSize: 30,
+                  ),
+                  topTitles: SideTitles(showTitles: false),
+                ),
+                borderData: FlBorderData(
+                  show: true,
+                  border: Border.all(color: Colors.black),
+                ),
+                barGroups: _generateBars(monthlyExpenses),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   List<BarChartGroupData> _generateBars(List<double> monthlyExpenses) {
     List<BarChartGroupData> bars = [];
@@ -224,13 +241,6 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   Widget _buildExpensesTab() {
-    List<double> monthlyExpenses = List.filled(12, 0.0);
-
-    for (var expense in _expenses) {
-      int month = expense['date'].month;
-      monthlyExpenses[month - 1] += expense['amount'];
-    }
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -251,15 +261,15 @@ class _MyHomePageState extends State<MyHomePage>
               final expense = _expenses[index];
               return ListTile(
                 title: Text(
-                  expense['title'],
+                  expense.title,
                   style: const TextStyle(fontFamily: 'BebasNeue'),
                 ),
                 subtitle: Text(
-                  'Amount: ₱${NumberFormat.currency(locale: 'en_PH', symbol: '').format(expense['amount'])}, Date: ${DateFormat('dd-MM-yyyy').format(expense['date'])}',
+                  'Amount: ${NumberFormat.currency(locale: 'en_PH', symbol: '').format(expense.amount)}, Date: ${DateFormat('dd-MM-yyyy').format(expense.date)}',
                   style: const TextStyle(fontFamily: 'BebasNeue'),
                 ),
                 trailing: IconButton(
-                  icon: Icon(Icons.delete),
+                  icon: const Icon(Icons.delete),
                   onPressed: () => _confirmDeleteExpense(index),
                 ),
               );
@@ -273,139 +283,103 @@ class _MyHomePageState extends State<MyHomePage>
   void _confirmDeleteExpense(int index) async {
     bool confirmDelete = await showDialog(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Confirm"),
-          content: const Text("Are you sure you want to delete this expense?"),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text("Delete"),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (confirmDelete) {
-      setState(() {
-        _totalMonthlyAmount -= _expenses[index]['amount'];
-        _expenses.removeAt(index);
-      });
-    }
-  }
-
-  void _showExpenseEntryDialog(BuildContext context) async {
-    final result = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return ExpenseEntryDialog();
-      },
-    );
-
-    if (result != null) {
-      String title = result['title'];
-      double amount = double.tryParse(result['amount'] ?? '') ?? 0.0;
-
-      if (amount <= 0) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Please enter a valid amount.'),
+      builder: (context) => AlertDialog(
+        title: const Text('Confirm Delete'),
+        content: const Text('Are you sure you want to delete this expense?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Delete'),
           ),
-        );
-        return;
-      }
-
-      DateTime date = DateTime.now();
-
-      setState(() {
-        _expenses.add({
-          'title': title,
-          'amount': amount,
-          'date': date,
-        });
-
-        _totalMonthlyAmount += amount;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-}
-
-class ExpenseEntryDialog extends StatefulWidget {
-  @override
-  _ExpenseEntryDialogState createState() => _ExpenseEntryDialogState();
-}
-
-class _ExpenseEntryDialogState extends State<ExpenseEntryDialog> {
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _amountController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Add Expense'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextField(
-            controller: _titleController,
-            decoration: const InputDecoration(labelText: 'Title'),
-          ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}$')),
-            ],
-            decoration: const InputDecoration(labelText: 'Amount'),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Cancel'),
           ),
         ],
       ),
-      actions: <Widget>[
-        ElevatedButton(
-          onPressed: () {
-            String title = _titleController.text.trim();
-            String amount = _amountController.text.trim();
+    );
 
-            if (title.isEmpty || amount.isEmpty) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Please enter valid data.'),
-                ),
-              );
-            } else {
-              Navigator.of(context).pop({
-                'title': title,
-                'amount': amount,
-              });
-            }
-          },
-          child: const Text('Save'),
+    if (confirmDelete) {
+      _deleteExpense(index);
+    }
+  }
+
+  void _deleteExpense(int index) async {
+    final expense = _expenses[index];
+    try {
+      await _expenseBox?.delete(expense.key); // Use the key field for deletion
+      _fetchExpenses(); // Update the list after deletion
+    } catch (e) {
+      print('Error deleting expense: $e');
+      // Handle error, show error message, etc.
+    }
+  }
+
+  Future<void> _showExpenseEntryDialog(BuildContext context) async {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Add Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: amountController,
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}'))],
+              decoration: const InputDecoration(labelText: 'Amount'),
+            ),
+          ],
         ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Close the dialog
-          },
-          child: const Text('Cancel'),
-        ),
-      ],
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              _addExpense(titleController.text, double.tryParse(amountController.text) ?? 0.0);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Add'),
+          ),
+        ],
+      ),
     );
   }
 
+  void _addExpense(String title, double amount) async {
+  String uniqueKey = DateTime.now().millisecondsSinceEpoch.toString(); // Generate a unique key
+  final expense = Expense(
+    key: uniqueKey,
+    title: title,
+    amount: amount,
+    date: DateTime.now(),
+  );
+
+  await _expenseBox?.add(expense);
+  _fetchExpenses();
+}
+
+
   @override
-  void dispose() {
-    _titleController.dispose();
-    _amountController.dispose();
-    super.dispose();
-  }
+void dispose() {
+  _tabController.dispose();
+  _expenseBox?.close(); // Close the Hive box properly
+  super.dispose();
+}
+
 }
